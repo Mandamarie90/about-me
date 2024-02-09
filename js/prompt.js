3c7b3512a35c80559bb26268f344e6a580a67cc1
Amanda <amandamarquez27@icloud.com>
2024-02-08 21:45:42 -0800

    adding js

--- js/prompt.js
+++ js/prompt.js
@@ -1,3 +1,14 @@
+function favColor(){
 let color= prompt("What is your favorite color?");
 
 document.write("Nice! " + color + " is cool.");
+}
+
+function doYouLikeCats() {
+    let userLikesCats = confirm ("Do you love cats?");
+    if(userLikesCats) {
+        return "We just became bestfriends!";
+    } else {
+        return "Aww.. thats so sad!";
+    }
+}
