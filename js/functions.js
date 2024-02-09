ee67d81080356537f1e3e7ad47e11b5f1d79c49c
Amanda <amandamarquez27@icloud.com>
2024-02-08 22:46:36 -0800

    fix spacing

--- js/functions.js
+++ js/functions.js
@@ -20,8 +20,8 @@ function multiInputAlert() {
     }
 // Add inputs into a single message
 var message = "Thank you, " + input1 + "\n";
-message +=  input2 + "for subscribing with email"+"\n";
-message += " " + input3+ ".";
+message +=  input2 + " for subscribing using email "+"\n";
+message +=  input3 + ".";
 
 // Display the message in an alert
 alert(message);
