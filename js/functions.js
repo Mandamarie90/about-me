74156989e426ff3442d2f0983d307ce58c82ab1c
Amanda <amandamarquez27@icloud.com>
2024-02-08 22:41:35 -0800

    add alert

--- js/functions.js
+++ js/functions.js
@@ -4,3 +4,25 @@ function greeting( name ) {
   }
   let officialGreeting = greeting("John");
 console.log(officialGreeting);
+
+
+function multiInputAlert() {
+    let input1 = prompt("Enter your first name:");
+    let input2 = prompt("Enter your last name:");
+    let input3 = prompt("Enter your email:");
+  
+    
+    if (!input1 || !input2 || !input3) {
+      // Display error message and prompt again
+      alert("Error: All inputs must be provided.");
+      multiInputAlert(); // Call the function recursively for a second chance
+      return; // Stop execution of the function
+    }
+// Add inputs into a single message
+var message = "Thank you, " + input1 + "\n";
+message +=  input2 + "for subscribing with email"+"\n";
+message += " " + input3+ ".";
+
+// Display the message in an alert
+alert(message);
+}
