7dbd65b17271c376d312ab2212f3f5011c93b509
Amanda <amandamarquez27@icloud.com>
2024-02-21 13:47:28 -0800

    Add lighouse image

--- js/script.js
+++ js/script.js
@@ -3,28 +3,28 @@
 function askQuestion(question, answer) {
   let userInput = prompt(question).toLowerCase();
   if (userInput === answer) {
-    alert("Correct!");
+    alert('Correct!');
   } else {
-    alert("Incorrect!");
+    alert('Incorrect!');
   }
 }
 
 function greetUser() {
-  let userName = prompt("What's your name?");
-  alert("Welcome to my site, " + userName + "!");
+  let userName = prompt('What is your name?');
+  alert('Welcome to my site, " + userName + "!');
   return userName;
 }
 
 let user = greetUser();
 
 function displayFinalMessage() {
-  alert("Thanks for playing, " + user + "! I hope you enjoyed learning about me.");
+  alert('Thanks for playing, ' + user + '! I hope you enjoyed learning about me.');
 }
 
-askQuestion("Did I receive my Medical Assistant certification in 2015? (y/n)", "y");
-askQuestion("Do I have 7 years of medical assisting experience? (y/n)", "y");
-askQuestion("Am I aiming to become a software engineer? (y/n)", "y");
-askQuestion("Is the sky blue? (y/n)", "y"); 
-askQuestion("Is the Earth flat? (y/n)", "n");
+askQuestion('Did I receive my Medical Assistant certification in 2015? (y/n)', 'y');
+askQuestion('Do I have 7 years of medical assisting experience? (y/n)', 'y');
+askQuestion('Am I aiming to become a software engineer? (y/n)', 'y');
+askQuestion('Is the sky blue? (y/n)', 'y');
+askQuestion('Is the Earth flat? (y/n)', 'n');
 
 displayFinalMessage();
